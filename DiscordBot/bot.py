e082615a09a4f8707c42208f2ad73d1c14ad7352
Raphael <raphael.t.ruban@gmail.com>
2023-05-17 01:22:28 -0700

    adds basic view flow

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -12,8 +12,10 @@ import pdb
 # Set up logging to the console
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 # There should be a file called 'tokens.json' inside the same folder as this file
@@ -27,13 +29,13 @@ with open(token_path) as f:
 
 
 class ModBot(discord.Client):
-    def __init__(self): 
+    def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
         super().__init__(command_prefix='.', intents=intents)
         self.group_num = None
-        self.mod_channels = {} # Map from guild to the mod channel id for that guild
-        self.reports = {} # Map from user IDs to the state of their report
+        self.mod_channels = {}  # Map from guild to the mod channel id for that guild
+        self.reports = {}  # Map from user IDs to the state of their report
 
     async def on_ready(self):
         print(f'{self.user.name} has connected to Discord! It is these guilds:')
@@ -46,21 +48,21 @@ class ModBot(discord.Client):
         if match:
             self.group_num = match.group(1)
         else:
-            raise Exception("Group number not found in bot's name. Name format should be \"Group # Bot\".")
+            raise Exception(
+                "Group number not found in bot's name. Name format should be \"Group # Bot\".")
 
         # Find the mod channel in each guild that this bot should report to
         for guild in self.guilds:
             for channel in guild.text_channels:
                 if channel.name == f'group-{self.group_num}-mod':
                     self.mod_channels[guild.id] = channel
-        
 
     async def on_message(self, message):
         '''
         This function is called whenever a message is sent in a channel that the bot can see (including DMs). 
         Currently the bot is configured to only handle messages that are sent over DMs or in your group's "group-#" channel. 
         '''
-        # Ignore messages from the bot 
+        # Ignore messages from the bot
         if message.author.id == self.user.id:
             return
 
@@ -73,7 +75,7 @@ class ModBot(discord.Client):
     async def handle_dm(self, message):
         # Handle a help message
         if message.content == Report.HELP_KEYWORD:
-            reply =  "Use the `report` command to begin the reporting process.\n"
+            reply = "Use the `report` command to begin the reporting process.\n"
             reply += "Use the `cancel` command to cancel the report process.\n"
             await message.channel.send(reply)
             return
@@ -91,8 +93,8 @@ class ModBot(discord.Client):
 
         # Let the report class handle this message; forward all the messages it returns to uss
         responses = await self.reports[author_id].handle_message(message)
-        for r in responses:
-            await message.channel.send(r)
+        for r, view in responses:
+            await message.channel.send(r, view=view)
 
         # If the report is complete or cancelled, remove it from our map
         if self.reports[author_id].report_complete():
@@ -109,7 +111,6 @@ class ModBot(discord.Client):
         scores = self.eval_text(message.content)
         await mod_channel.send(self.code_format(scores))
 
-    
     def eval_text(self, message):
         ''''
         TODO: Once you know how you want to evaluate messages in your channel, 
@@ -117,14 +118,13 @@ class ModBot(discord.Client):
         '''
         return message
 
-    
     def code_format(self, text):
         ''''
         TODO: Once you know how you want to show that a message has been 
         evaluated, insert your code here for formatting the string to be 
         shown in the mod channel. 
         '''
-        return "Evaluated: '" + text+ "'"
+        return "Evaluated: '" + text + "'"
 
 
 client = ModBot()
--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -1,6 +1,8 @@
 from enum import Enum, auto
 import discord
+from discord.ui import Button, View, Select
 import re
+
 
 class State(Enum):
     REPORT_START = auto()
@@ -8,16 +10,21 @@ class State(Enum):
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
 
+
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
 
+    ABUSE_TYPES = ["Bullying or harassment", "Scam or fraud", "Suicide or self-injury",
+                   "Violence or dangerous organizations", "Hate speech or symbols", "Nudity or sexual activity", "Spam", "Other reason"]
+
     def __init__(self, client):
         self.state = State.REPORT_START
         self.client = client
         self.message = None
-    
+        self.abuse_type = None
+
     async def handle_message(self, message):
         '''
         This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
@@ -27,46 +34,57 @@ class Report:
 
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
-            return ["Report cancelled."]
-        
+            return [("Report cancelled.", None)]
+
         if self.state == State.REPORT_START:
-            reply =  "Thank you for starting the reporting process. "
+            reply = "Thank you for starting the reporting process. "
             reply += "Say `help` at any time for more information.\n\n"
             reply += "Please copy paste the link to the message you want to report.\n"
             reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
             self.state = State.AWAITING_MESSAGE
-            return [reply]
-        
+            return [(reply, None)]
+
         if self.state == State.AWAITING_MESSAGE:
             # Parse out the three ID strings from the message link
             m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
             if not m:
-                return ["I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."]
+                return [("I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel.", None)]
             guild = self.client.get_guild(int(m.group(1)))
             if not guild:
-                return ["I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."]
+                return [("I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again.", None)]
             channel = guild.get_channel(int(m.group(2)))
             if not channel:
-                return ["It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return [("It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel.", None)]
             try:
                 message = await channel.fetch_message(int(m.group(3)))
             except discord.errors.NotFound:
-                return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return [("It seems this message was deleted or never existed. Please try again or say `cancel` to cancel.", None)]
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
-        
+            found_msgs = [("I found this message:", None), ("```" +
+                                                            message.author.name + ": " + message.content + "```", None)]
+
+            # Give options to report
+            # button = Button(label="Click me", style=discord.ButtonStyle.green)
+            abuse_type_select = Select(
+                placeholder="Select abuse type...", options=[discord.SelectOption(label=abuse) for abuse in self.ABUSE_TYPES])
+
+            async def select_callback(interaction):
+                self.abuse_type = interaction.data["values"][0]
+                await interaction.response.edit_message(view=None)
+                await interaction.followup.send("You selected " + interaction.data["values"][0].lower() + ".")
+            
+            abuse_type_select.callback = select_callback
+            view = View()
+            view.add_item(abuse_type_select)
+
+            return [*found_msgs, ("Why would you like to report this message?", view)]
+
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            return [("<insert rest of reporting flow here>", None)]
 
         return []
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
-    
-
-
-    
-
