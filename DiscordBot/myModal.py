4091d9042b51e05add12cf5229e26c8ec9162385
Matthew Early <matthew.ergc.lin.wy@gmail.com>
2023-05-17 21:06:44 -0700

    Try again to get buttons to appear

--- DiscordBot/myModal.py
+++ DiscordBot/myModal.py
@@ -11,8 +11,12 @@ from discord import ui
 class MyModal(ui.Modal):
 	question1 = ui.TextInput(label="This is a question with short text input", style=discord.TextStyle.short)
 	question2 = ui.TextInput(label="This is a question with long text input", style=discord.TextStyle.long)
-	button1 = ui.Button(label="It's a button", custom_id=button1)
-	button2 = ui.Button(label="Wow", custom_id=button2)
+	@ui.button(label="It's a button")
+	async def buttonMethod1(self, interaction: discord.Interaction, Button: ui.Button):
+		await interaction.response.send_message("Pressed button 1")
+	@ui.button(label="Wow")
+	async def buttonMethod2(self, interaction: discord.Interaction, Button: ui.Button):
+		await interaction.response.send_message("Pressed button 2")
 
 	async def on_submit(self, interaction: discord.Interaction):
 		await interaction.response.send_message("You filled out the form.")
