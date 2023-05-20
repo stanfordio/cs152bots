7fb53b4e1e521a032490e7b0de3a6456be0df3d1
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2023-05-19 21:50:58 -0700

    Update additional selection based on flowchart - creation up to "Do you know the user responsible" (left branch flowchart) but not "Do you know this other person" right branch. Added global tickets dict as temporary database for selections

--- DiscordBot/mainMenu.py
+++ DiscordBot/mainMenu.py
@@ -1,8 +1,13 @@
 import discord
 from myModal import MyModal
+import uuid
 
+tickets = {}
 
-class ReportSelection(discord.ui.View):   
+class ReportSelection(discord.ui.View): 
+    def __init__(self, tid):
+        super().__init__()
+        self.tid = tid
     @discord.ui.select(placeholder='Please select reason for reporting this content', options=[
             discord.SelectOption(label='Harassment', description='description1'),
             discord.SelectOption(label='Spam', description='description2'),
@@ -11,11 +16,16 @@ class ReportSelection(discord.ui.View):
             discord.SelectOption(label='Other', description='description5')
         ])
     async def selection_callback(self, interaction:discord.Interaction, selection:discord.ui.Select):
-        await interaction.response.send_message(f'You chose {selection.values[0]}')
+        await interaction.response.send_message(f'You chose {selection.values[0]}',  ephemeral=True)
+        tickets[self.tid] = {'reason': selection.values[0]}
         if selection.values[0] == 'Harassment':
-            await interaction.followup.send(view=HarassmentSelection())
+            await interaction.followup.send(view=HarassmentSelection(self.tid),  ephemeral=True)
 
 class HarassmentSelection(discord.ui.View):
+    def __init__(self, tid):
+        super().__init__()
+        self.tid = tid
+
     @discord.ui.select(placeholder='Select Type', options=[
             discord.SelectOption(label='Sextortion', description='description1'),
             discord.SelectOption(label='Hate Speech', description='description2'),
@@ -24,9 +34,56 @@ class HarassmentSelection(discord.ui.View):
             discord.SelectOption(label='Other', description='description5')
         ])
     async def selection_callback(self, interaction:discord.Interaction, selection:discord.ui.Select):
-        await interaction.response.send_message(f'You chose {selection.values[0]}')
+        tickets[self.tid]['harassment_type'] = selection.values[0]
 
+        await interaction.response.send_message(f'You chose {selection.values[0]}',  ephemeral=True)
+        if selection.values[0] == 'Sextortion':
+            await interaction.followup.send(view=SextortionTypeSelection(self.tid),  ephemeral=True)
 
+class SextortionTypeSelection(discord.ui.View):
+    def __init__(self, tid):
+        super().__init__()
+        self.tid = tid
+
+    @discord.ui.select(placeholder='Select Type of Content', options=[
+            discord.SelectOption(label='Content includes explicit images', description='description1'),
+            discord.SelectOption(label='Content is a threat to spread explicit images', description='description2'),
+        ])
+    async def sextortype_callback(self, interaction:discord.Interaction, selection:discord.ui.Select):
+        tickets[self.tid]['sextortion_content'] = selection.values[0]
+        await interaction.response.send_message(f'You chose {selection.values[0]}',  ephemeral=True)
+        await interaction.followup.send('Are these images of you or someone else?', view=ImageOwnerSelection(self.tid),  ephemeral=True)
+
+class ImageOwnerSelection(discord.ui.View):
+    def __init__(self, tid):
+        super().__init__()
+        self.tid = tid
+
+    @discord.ui.button(label='Me', style=discord.ButtonStyle.red)
+    async def MeOwnerBtn(self, interaction: discord.Interaction, button:discord.ui.Button):
+        tickets[self.tid]['image_owner'] = 'Me'
+        await interaction.response.send_message("Do you know the user responsible?", view=UserResponsibleSelection(self.tid), ephemeral=True)
+    @discord.ui.button(label="Other", style=discord.ButtonStyle.red)
+    async def OtherOwnerBtn(self, interaction: discord.Interaction, button:discord.ui.Button):
+        tickets[self.tid]['image_owner'] = 'Other'
+        await interaction.response.send_message("You clicked the help button. We've sent your request to the mod-team", ephemeral=True)
+
+class UserResponsibleSelection(discord.ui.View):
+    def __init__(self, tid):
+        super().__init__()
+        self.tid = tid
+
+    async def owner_choice_callback(self, button:discord.ui.Button, interaction:discord.Interaction):
+        tickets[self.tid]['image_owner'] = button.custom_id
+
+    @discord.ui.button(label="Yes", style=discord.ButtonStyle.red, custom_id='Me')
+    async def MeOwnerBtn(self, interaction: discord.Interaction, button:discord.ui.Button):
+        tickets[self.tid]['know_responsible'] = 'Yes'
+        await interaction.response.send_message("You clicked the help button. We've sent your request to the mod-team", ephemeral=True)
+    @discord.ui.button(label="No", style=discord.ButtonStyle.red, custom_id='Other')
+    async def OtherOwnerBtn(self, interaction: discord.Interaction, button:discord.ui.Button):
+        tickets[self.tid]['know_responsible'] = 'No'
+        await interaction.response.send_message("You clicked the help button. We've sent your request to the mod-team", ephemeral=True)
 class MainMenuEmbed(discord.Embed):
     def __init__(self):
         super().__init__()
@@ -47,7 +104,8 @@ class MainMenuButtons(discord.ui.View):
     @discord.ui.button(label="Report", style=discord.ButtonStyle.red)
     async def reportBtn(self, interaction: discord.Interaction, button:discord.ui.Button):
         # await interaction.response.send_modal(MyModal())
-        await interaction.response.send_message(view=ReportSelection())
+        tid = uuid.uuid4()
+        await interaction.response.send_message(view=ReportSelection(tid))
     
     # @discord.ui.button(label="Help", style=discord.ButtonStyle.red)
     # async def helpBtn(self, interaction: discord.Interaction, button:discord.ui.Button):
