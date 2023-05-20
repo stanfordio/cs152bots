a98c2c403d8edd1f0e1c3f4c49aeb903a96b7216
Raphael <raphael.t.ruban@gmail.com>
2023-05-19 22:53:31 -0700

    adds report state

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -1,7 +1,8 @@
 from enum import Enum, auto
 import discord
 import re
-from user_report import StartView
+from user_report import StartView, ABUSE_TYPES, HARASSMENT_TYPES
+from typing import Optional, List
 
 
 class State(Enum):
@@ -17,17 +18,23 @@ class Report:
     HELP_KEYWORD = "help"
 
     def __init__(self, client):
+        # State to handle inner working of bot
         self.state = State.REPORT_START
         self.client = client
+        # State for filing a report
         self.message = None
-        self.abuse_type = None
+        self.abuse_type: Optional[ABUSE_TYPES] = None
+        self.harassment_type: List[HARASSMENT_TYPES] = []
+        self.target = None  # TODO: Still has to change
+        self.additional_msg_ids: List[str] = None
+        self.additional_info: Optional[str] = None
 
     async def handle_message(self, message):
-        '''
-        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
+        """
+        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what
         prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
-        get you started and give you a model for working with Discord. 
-        '''
+        get you started and give you a model for working with Discord.
+        """
 
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
@@ -43,30 +50,43 @@ class Report:
 
         if self.state == State.AWAITING_MESSAGE:
             # Parse out the three ID strings from the message link
-            m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
+            m = re.search("/(\d+)/(\d+)/(\d+)", message.content)
             if not m:
-                return ["I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."]
+                return [
+                    "I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."
+                ]
             guild = self.client.get_guild(int(m.group(1)))
             if not guild:
-                return ["I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."]
+                return [
+                    "I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."
+                ]
             channel = guild.get_channel(int(m.group(2)))
             if not channel:
-                return ["It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return [
+                    "It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel."
+                ]
             try:
                 message = await channel.fetch_message(int(m.group(3)))
             except discord.errors.NotFound:
-                return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return [
+                    "It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."
+                ]
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
-            found_msgs = ["I found this message:", "```" + message.author.name + ": " + message.content + "```"]
+            found_msgs = [
+                "I found this message:",
+                "```" + message.author.name + ": " + message.content + "```",
+            ]
 
             # The report flow is handled as a series of views. No extra states needed for now.
             view = StartView()
             return [*found_msgs, ("Why would you like to report this message?", view)]
 
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["Sorry, you are in the middle of a report.\nContinue by selecting an option above or stop by typing `cancel`."]
+            return [
+                "Sorry, you are in the middle of a report.\nContinue by selecting an option above or stop by typing `cancel`."
+            ]
 
         return []
 
