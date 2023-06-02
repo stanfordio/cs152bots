f45641cd21797d7eb4ead1abc6f9c126b71669b8
Your Name <you@example.com>
2023-06-02 15:26:56 -0700

    Finished roc_auc and confusion matrix generation

--- gpt4_classification.py
+++ gpt4_classification.py
@@ -1,26 +1,34 @@
 import openai
 import os
+import time
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORGANIZATION")
 
 def gpt4_classify_email(sms):
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a system that classifies messages as either being spam or not spam",
-            },
-            {"role": "user", "content": sms},
-        ],
-    )
-    print(f"Finished generating label")
-    classification = response["choices"][0]["message"]["content"].strip()
-    if classification == "spam":
-        return "spam"
-    else:
-        return "not spam"
+    gotAnswer = False
+    while gotAnswer == False: 
+        try:
+            response = openai.ChatCompletion.create(
+                model="gpt-4",
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a system that classifies messages as either being spam or not spam",
+                    },
+                    {"role": "user", "content": sms},
+                ],
+            )
+            gotAnswer = True
+            
+            classification = response["choices"][0]["message"]["content"].strip()
+            if classification == "spam":
+                return "spam"
+            else:
+                return "not spam"
+        except:
+            print("Error, trying again") 
+            time.sleep(0.005)
 
 comments = [
     'Hey John, just wanted to check in on you to see how your doing. Hows the move been?',
@@ -30,4 +38,4 @@ comments = [
 	'This is the Federal Investigations Department. IRS records show that there are a number of overseas transactionsunder your name, you need to pay the full portion of the transaction fees to the IRS Department, which you never did. You currently owe $5,638.38. Please call us as soon as possible at (415)-555-3437.'
 ]
 
-print(gpt4_classify_email(comments[1]))
+# print(gpt4_classify_email(comments[1]))
--- rocauc_generator.py
+++ rocauc_generator.py
@@ -2,11 +2,12 @@ from perspective import perspective_spam_prob
 import os
 import numpy as np
 from sklearn import metrics
+import time
+from gpt4_classification import gpt4_classify_email
+from custom_spam_classifier.custom_classifier import custom_classify_spam
+from tqdm import tqdm
 
-## Import messages as an array
-spam_file_path = "DatasetGeneration/spam_emails"
-ham_file_path = "DatasetGeneration/non_spam_emails"
-
+##### Helper Functions #####
 def getMessages(path):
     owd = os.getcwd()
     os.chdir(path)
@@ -22,32 +23,101 @@ def getMessages(path):
     os.chdir(owd)
     return messages
 
-spam_messages = getMessages(spam_file_path)
-ham_messages = getMessages(ham_file_path)
+# Rate Limited by Perspective to 1 / second
+def getPerspectivePredictions(messages):
+    predictions = []
+    for msg in tqdm(messages):
+        time.sleep(1)
+        predictions.append(perspective_spam_prob(msg))
+    return np.array(predictions)
 
-## Assign 0 for ham, 1 for spam - true classification
-spam_classifiers = np.array([1] * len(spam_messages))
-ham_classifiers = np.array([0] * len(ham_messages))
+def getGPT_Predictions(messages, gpt_func):
+    predictions = []
+    for msg in tqdm(messages):
+        # time.sleep() if needed
+        classified = gpt_func(msg)
+        if classified == 'spam':
+            predictions.append(1)
+        else:
+            predictions.append(0)
+    return np.array(predictions)
 
+##### Generate ROC/AUC for Perspective #####
 ## Run perspective.perspective_spam_prob(msg) on every message - predictions
-spam_predictions = np.array(list(map(perspective_spam_prob, spam_messages)))
-ham_predictions = np.array(list(map(perspective_spam_prob, ham_messages)))
+def printROC_AUC_Perspective(spam_messages, ham_messages, all_classifiers):
+    print("Perspective - SPAM")
+    spam_predictions = getPerspectivePredictions(spam_messages)
+    print("Perspective - HAM")
+    ham_predictions = getPerspectivePredictions(ham_messages)
 
-## Each message should have a corresponding classification and spam_prediction
-### Run these through sklearn.metrics.roc_curve(y_true, y_score)
-all_classifiers = np.concatenate((spam_classifiers, ham_classifiers))
-all_predictions = np.concatenate((spam_predictions, ham_predictions))
-fpr, tpr, thresholds = metrics.roc_curve(all_classifiers, all_predictions)
-roc_auc = metrics.auc(fpr, tpr)
+    ## Each message should have a corresponding classification and spam_prediction
+    ### Run these through sklearn.metrics.roc_curve(y_true, y_score)
+    perspective_predictions = np.concatenate((spam_predictions, ham_predictions))
+    np.savetxt('perspective_predictions.csv', np.column_stack((all_classifiers,perspective_predictions)), delimiter=',')
 
-# method I: plt
-import matplotlib.pyplot as plt
-plt.title('Receiver Operating Characteristic')
-plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
-plt.legend(loc = 'lower right')
-plt.plot([0, 1], [0, 1],'r--')
-plt.xlim([0, 1])
-plt.ylim([0, 1])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-plt.show(block=True)
+    # Generate chart
+    fpr, tpr, thresholds = metrics.roc_curve(all_classifiers, perspective_predictions)
+    roc_auc = metrics.auc(fpr, tpr)
+
+    np.savetxt('DataAnalysis/perspective_analysis.csv', np.column_stack((np.array(fpr), np.array(tpr), np.array(thresholds))), delimiter=',')
+
+
+    # Plots the ROC / AUC for Perspective
+    import matplotlib.pyplot as plt
+    plt.title('Receiver Operating Characteristic')
+    plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+    plt.legend(loc = 'lower right')
+    plt.plot([0, 1], [0, 1],'r--')
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show(block=True)
+
+##### Get Confusion Matrix for gpt4-classifier
+def printGPT4_Confusion(spam_messages, ham_messages, all_classifiers):
+    print("GPT4 - SPAM")
+    gpt_4_spam_predictions = getGPT_Predictions(spam_messages, gpt4_classify_email )
+    print("GPT4 - HAM")
+    gpt_4_ham_predictions = getGPT_Predictions(ham_messages, gpt4_classify_email)
+
+    gpt_4_predictions = np.concatenate((gpt_4_spam_predictions, gpt_4_ham_predictions))
+    
+    np.savetxt('DataAnalysis/gpt4_predictions.csv', np.column_stack((all_classifiers, gpt_4_predictions)), delimiter=',')
+
+    print(metrics.confusion_matrix(all_classifiers, gpt_4_predictions))
+
+##### Get Confusion Matrix for custom gpt3-classifier
+def printGPT3_Confusion(spam_messages, ham_messages, all_classifiers):
+    print("GPT3 - SPAM")
+    gpt_3_spam_predictions = getGPT_Predictions(spam_messages, custom_classify_spam)
+    print("GPT3 - HAM")
+    gpt_3_ham_predictions = getGPT_Predictions(ham_messages, custom_classify_spam)
+
+    gpt_3_predictions = np.concatenate((gpt_3_spam_predictions, gpt_3_ham_predictions))
+    
+    np.savetxt('DataAnalysis/gpt3_predictions.csv', np.column_stack((all_classifiers, gpt_3_predictions)), delimiter=',')
+
+    print(metrics.confusion_matrix(all_classifiers, gpt_3_predictions))
+
+
+def main():
+    ##### Grab Messages + Generate True Classification Matrix #####
+    spam_file_path = "DatasetGeneration/spam_emails"
+    ham_file_path = "DatasetGeneration/non_spam_emails"
+
+    ## Import messages as an array
+    spam_messages = getMessages(spam_file_path)
+    ham_messages = getMessages(ham_file_path)
+
+    ## Assign 0 for ham, 1 for spam - true classification
+    spam_classifiers = np.array([1] * len(spam_messages))
+    ham_classifiers = np.array([0] * len(ham_messages))
+    all_classifiers = np.concatenate((spam_classifiers, ham_classifiers))
+
+    # printGPT4_Confusion(spam_messages, ham_messages, all_classifiers)
+    # printROC_AUC_Perspective(spam_messages, ham_messages, all_classifiers)
+
+
+if __name__ == '__main__':
+    main()
