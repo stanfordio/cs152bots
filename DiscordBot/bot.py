f0f1b572f30f5de40ec2f4be9dfbf6876962e8f7
Frankie <fmcerk@stanford.edu>
2023-06-04 09:36:38 -0700

    bot functional but not thoroughly tested

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -14,6 +14,120 @@ openai.organization = "org-YVZe9QFuR0Ke0J0rqr7l2R2L"
 from report import Report, BotReactMessage
 import pdb
 from queue import PriorityQueue
+# from mod_flow import new_report
+
+# Stuff from mod_flow
+
+####################################################################################################
+
+HIGHEST_PRI = 1
+HIGH_PRI = 2
+MED_PRI = 3
+LOW_PRI = 4
+
+def new_report(completed_report, user_being_reported, user_making_report, reports_by_user, reports_about_user):
+
+    # Check abuse type
+    reported_issues = completed_report.get_reported_issues()
+    decisions = []
+
+    if Report.HARASSMENT in reported_issues:
+        decisions.append(harassment_report(user_being_reported, reports_about_user))
+    elif Report.SPAM in reported_issues:
+        decisions.append(spam_report(reports_about_user[user_being_reported]))
+    elif  Report.THREAT in reported_issues[0]:
+        decisions.append(threat_report(completed_report))
+    elif Report.FRAUD in reported_issues[0]:
+        decisions.append(fraud_report(completed_report))
+    else:  # Other
+        decisions.append(other_report(completed_report))
+
+    # pick most important decision:
+    final_decision, final_message, final_pri = "NO_ACTION", "", LOW_PRI
+    for decision in decisions:
+        d, m, p = decision
+        if d == "BAN" and final_decision != "BAN":
+             final_decision, final_message, final_pri = d, m, p
+        elif d == "SUSPEND" and (final_decision == "NO_ACTION" or final_decision == "MANUAL"):
+             final_decision, final_message, final_pri = d, m, p
+        elif d == "MANUAL" and final_decision == "MANUAL":
+             final_decision, final_message, final_pri = d, m, p
+        if d == final_decision and p < final_pri:
+             final_decision, final_message, final_pri = d, m, p
+
+    return final_decision, final_message, final_pri
+
+
+def harassment_report(completed_report, list_of_reports_against_user):
+    is_harrassment = spam(completed_report.get_reported_message())
+
+    harrassment_count = 0
+    for report in list_of_reports_against_user:
+        if Report.HARASSMENT in report.reported_issues:
+            harrassment_count += 1
+
+    if is_harrassment and harassment_count < 3:
+        'SUSPEND', "Your message was marked as harmful, you have been suspended " \
+                          "for 15 days. Please contact us if you think we made a mistake.", MED_PRI
+    elif harrassment_count >= 3 and is_harrassment:  # many reports of harassment
+        return 'BAN', "Your account has been banned due to too many harmful messages.", MED_PRI
+    elif harrassment_count > 5:  # many reports of harassment
+        return 'SUSPEND', "You have been suspended for 15 days due to harmful messages. Please contact us if you think we made a mistake.", MED_PRI
+    else:
+        return  "MANUAL", "", MED_PRI
+
+
+def spam_report(completed_report, list_of_reports_against_user):
+    spam_count = 0
+    is_spam = spam(completed_report.get_reported_message())
+    for report in list_of_reports_against_user:
+        if  Report.SPAM in report.reported_issues[0]:
+            spam_count += 1
+
+    if is_spam and spam_count >= 3:  # user has been reported many times for spam
+        return 'BAN', "Your account has been banned due to too many spam messages.", MED_PRI
+    elif is_spam and spam_count < 3:
+        return 'SUSPEND', "Your account has been suspended due to reports of spam messages. ", MED_PRI
+    else:
+        return 'NO ACTION', ": We did not find this message to be abusive. " \
+                          "Please contact us if you think we made a mistake.", LOW_PRI
+
+
+def threat_report(completed_report):
+    is_threat = threat(completed_report.get_reported_message)
+    if is_threat:
+        return 'BAN', "Your account has been banned due to harmful messages.", HIGH_PRI
+    return 'MANUAL', "", HIGHEST_PRI
+
+
+def fraud_report(completed_report):
+    reported_issues = completed_report.get_reported_issues()
+
+    if Report.REQUESTED_MONEY in reported_issues:
+        # if Report.OBTAINED_MONEY in reported_issues:  # requested and obtained money
+        #     return 'MANUAL', "Your account is under review due to reports of fraud.", HIGH_PRI
+        # else:  # requested but did not obtain money
+        if money_message(completed_report.get_reported_message()) == "yes":
+            return 'BAN', "Your account has been banned due to reports of monetary fraud.", HIGH_PRI
+        else:
+            return 'MANUAL', "Your account is under review due to reports of fraud", HIGHEST_PRI
+    elif Report.IMPERSONATION in reported_issues:
+        if impersonating(completed_report.get_reported_message()) == "yes":
+            return 'BAN', "Your account has been banned due to reports of impersonation.", HIGH_PRI
+        else:
+            return 'MANUAL', "Your account is under review due to reports of fraud.", MED_PRI
+
+    elif Report.FALSE_INFO in reported_issues:
+        return 'MANUAL', "Your account is under review due to reports of fraud.", MED_PRI
+    else:
+        return other_report(completed_report)
+
+
+def other_report(completed_report):
+    response = "Your report has been placed in our queue for manual moderation."
+    return 'MANUAL', response, LOW_PRI
+
+####################################################################################################
 
 # Set up logging to the console
 logger = logging.getLogger('discord')
@@ -30,7 +144,7 @@ with open(token_path) as f:
     # If you get an error here, it means your token is formatted incorrectly. Did you put it in quotes?
     tokens = json.load(f)
     discord_token = tokens['discord']
-    openai.api_key = tokens['open-api']
+    openai.api_key = tokens['openai']
 
 def money_message(message):
         prompt = "Is the person asking for money? \nMessage: " + message + "\nAnswer:"
@@ -253,7 +367,7 @@ class ModBot(discord.Client):
                 self.reports_about_user[user_being_reported] = []
             self.reports_about_user[user_being_reported].append(completed_report)
 
-            decision, message, severity = mod_flow.new_report(completed_report, user_being_reported,
+            decision, message, severity = new_report(completed_report, user_being_reported,
                                                           user_making_report, self.reports_by_user,
                                                           self.reports_about_user)
             if decision == "MANUAL":
@@ -276,7 +390,7 @@ class ModBot(discord.Client):
                                                                                                 "Thank you for keeping our platform safe.")
                     self.in_prog_reviews.pop(msg_id)
 
-                elif decision == "SUSPEND"':  # Suspend for 15 days
+                elif decision == "SUSPEND":  # Suspend for 15 days
                     user_reported = await self.fetch_user(user_being_reported)
                     reporter = await self.fetch_user(user_making_report)
                     await user_reported.send("We detected fraudulent activity on your account. "
@@ -291,8 +405,8 @@ class ModBot(discord.Client):
                     await reporter.send(
                         "We did not detect any fraudulent information in the message/profile submitted. "
                         "The next course of action to protect yourself can include: " + "\n"
-                                                                                    "\t" + "Blocking " + user_reported.name + "\n"
-                                                                                                                            "\t" + "Calling 911 if this is an emergency."
+                                                                                    + "\t" + "Blocking " + user_reported.name + "\n"
+                                                                                                                            + "\t" + "Calling 911 if this is an emergency.")
         # for r in responses:
         #   await message.channel.send(r)
 
@@ -442,3 +556,5 @@ class ModBot(discord.Client):
 
 client = ModBot()
 client.run(discord_token)
+
+
