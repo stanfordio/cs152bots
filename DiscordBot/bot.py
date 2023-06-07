a2e08f45a854c756efe7ae75301d04bd557fa13b
Justin Tinker <justin@Justins-MacBook-Air.local>
2023-06-07 07:00:07 -0700

    t

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -218,7 +218,9 @@ class ModBot(discord.Client):
 
 
     async def handle_channel_message(self, message):
-        print("c")
+        if message.channel.name == f'group-{self.group_num}-mod':
+            return self.handle_mod_channel_message(message)
+        
         # Only handle messages sent in the "group-#" channel
         if not message.channel.name == f'group-{self.group_num}':
             return
