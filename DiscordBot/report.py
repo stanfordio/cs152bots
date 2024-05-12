744e2bab67d5cf8fbabdee1193f01b04e3790eec
selenasun1618 <selenasun02@gmail.com>
2024-05-12 00:50:19 -0700

    added options, corrected logic

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -5,11 +5,11 @@ import re
 
 # options given to users to select from to make 
 SPECIFIC_OPTIONS = {
-    1: "Please select the type of imminent danger you see:\n1. Credible threat of violence\n 2. Suicidal content or self-harm",
-    2: "Please select the type of spam you see:\n1. Fraud\n2. Solicitation\n3. Impersonation",
+    1: "Please select the type of imminent danger you see:\n1. Credible threat of violence\n 2. Suicidal content or self-harm\n3.Other",
+    2: "Please select the type of spam you see:\n1. Fraud\n2. Solicitation\n3. Impersonation\n4.Other",
     3: "Please select the type of nudity or graphic content you see:\n1. Directed at you (e.g., porn, violence, sextortion)\n2. Directed at a minor\n3. Harassment\n4. Other",
-    4: "Please select the type of disinformation you see:\n1. Targeted at political candidates/figures\n2. Imposter\n3. False context\n4. Fabricated content",
-    5: "Please select the type of hate speech/harrassment you see:\n1. Bullying\n2. Hate speech directed at me/specific group of people\n3. Unwanted sexual content\n4. Revealing Private Information",
+    4: "Please select the type of disinformation you see:\n1. Directed at a political figure\n2. Directed at a private individual\n3. Directed at an organization \n4. Other",
+    5: "Please select the type of hate speech/harrassment you see:\n1. Bullying\n2. Hate speech directed at me/specific group of people\n3. Unwanted sexual content\n4. Revealing Private Information\n5.Other",
 }
 
 # closing messages given to the user, before their report is processed
@@ -35,11 +35,11 @@ ABUSE_STRINGS = {
 
 # for the second level response
 SPECIFIC_ABUSE_STRINGS = {
-    1: ["Credible threat of violence", "Suicidal content or self-harm"],
-    2: ["Fraud", "Solicitation", "Impersonation"],
+    1: ["Credible threat of violence", "Suicidal content or self-harm", "Other"],
+    2: ["Fraud", "Solicitation", "Impersonation", "Other"],
     3: ["Of you (revenge porn or sextortion)", "Of a minor", 'Harassment', "Other"],
-    4: ["Targeted at political candidates/figures", "Imposter", "False context", "Fabricated content"],
-    5: ["Bullying", "Hate speech directed at me/specific group of people", "Unwanted sexual content", "Revealing Private Information"]
+    4: ["Directed at a political figure", "Directed at a private indivdual", "Directed at an organization", "Other"],
+    5: ["Bullying", "Hate speech directed at me/specific group of people", "Unwanted sexual content", "Revealing private information", "Other"]
 }
 
 class State(Enum):
@@ -158,7 +158,7 @@ class Report:
             except:
                 return [SPECIFIC_OPTIONS[self.abuse_type]]
             
-            if selection < 1 or (selection > 2 and self.abuse_type == 1) or (selection > 3 and self.abuse_type == 2 ) or (selection > 4 and self.abuse_type >=3):
+            if selection < 1 or (selection > 3 and self.abuse_type == 1) or (selection > 5 and self.abuse_type == 5) or (selection > 4 and (self.abuse_type == 2 or self.abuse_type == 3 or self.abuse_type == 4)):
                 return ["Please type a valid number of the content type you see."]
             
             if self.abuse_type == 4 or self.abuse_type == 5:
