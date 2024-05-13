4498f47b2c31878ce7fbcce75a9a11114e30e257
Lara Franciulli <larafranciulli@DN0a1f28dd.SUNet>
2024-05-13 14:20:48 -0700

    fixed merge conflicts

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -50,6 +50,15 @@ class Report:
         "They asked for intimate images of me or someone else",
         "Something else"
     ]
+    REPORT_LEGIT_OPTIONS = [
+        "Yes",
+        "No",
+        "Need more information"
+    ]
+    VIOLATION_OPTIONS = [
+        "It does not violate policy/the law, but not appropriate behavior (e.g., text a minor saying they are cute)",
+        "Escalate to law enforcement",
+    ]
 
     REPORT_COMPLETE_OTHER_MESSAGE = "Thank you for helping us keep our community safe! We will investigate the matter and follow up as needed."
     REPORT_COMPLETE_SEXTORTION_MESSAGE = '''Thank you for helping us keep our community safe! We will investigate the matter and follow up as needed.
@@ -216,7 +225,10 @@ class Report:
             if i == 1:
                 # TODO: add no to moderator report
                 pass
-              
+            self.state = State.AWAITING_FINAL_ADDITIONAL_INFORMATION
+            reply = f"Please add any additional information you think is relevant ({self.EXPLANATION_INPUT_LIMIT} word limit)."
+            return [reply]
+        
         if self.state == State.AWAITING_EXPLANATION_INPUT:
             if len(message.content.split()) > self.EXPLANATION_INPUT_LIMIT:
                 reply = f"Please do not exceed the {self.EXPLANATION_INPUT_LIMIT} word limit."
@@ -247,6 +259,7 @@ class Report:
             return reply
 
         if self.state == State.AWAITING_BLOCK_ANSWER:
+            reply = ""
             i = self.get_index(message, self.YES_NO_OPTIONS)
             if i == -1:
                 return ["Please enter a number corresponding to the given options."]
@@ -261,6 +274,49 @@ class Report:
 
         return []
 
+    async def handle_report(self, report):
+        '''
+        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
+        prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
+        get you started and give you a model for working with Discord. 
+        '''
+
+        if message.content == self.CANCEL_KEYWORD:
+            self.state = State.REPORT_COMPLETE
+            return ["Report cancelled."]
+
+        if self.state == State.REPORT_START:
+            self.state = State.AWAITING_REPORT_LEGIT
+            return [self.create_options_list("Is report legit?",
+                                             self.REPORT_LEGIT_OPTIONS)]
+
+        if self.state == State.AWAITING_REPORT_LEGIT:
+            i = self.get_index(message, self.REPORT_LEGIT_OPTIONS)
+            self.reason.append(self.REPORT_LEGIT_OPTIONS[i])
+
+            if i == -1:
+                return ["Please enter a number corresponding to the given options."]
+            if i == 0:
+                self.state = State.AWAITING_VIOLATION_DECISION
+                reply = self.create_options_list("Please select how to proceed:",
+                                                 self.VIOLATION_OPTIONS)
+            elif i == 1:
+                pass
+            else:
+                pass
+            return [reply]
+
+        if self.state == State.AWAITING_VIOLATION_DECISION:
+            i = self.get_index(message, self.VIOLATION_OPTIONS)
+            self.reason.append(self.VIOLATION_OPTIONS[i])
+            if i == -1:
+                return ["Please enter a number corresponding to the given options."]
+            if i == 0:
+                pass
+            elif i == 1:
+                pass
+        return []
+    
     def create_options_list(self, prompt, options):
         res = prompt
         for i, option in enumerate(options):
