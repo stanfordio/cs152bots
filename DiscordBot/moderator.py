2960c2a16e45480a3968c223324079f757d8283b
faebluna <100067923+faebluna@users.noreply.github.com>
2024-06-04 23:41:28 -0700

    supabase integration to moderator and report flow

--- DiscordBot/moderator.py
+++ DiscordBot/moderator.py
@@ -2,15 +2,9 @@ from enum import Enum, auto
 import discord
 import json
 import re
-from supabase import create_client, Client
+from supabase_client import SupabaseClient
 
-with open("tokens.json", "r") as f:
-    tokens = json.load(f)
-
-supabase_url = tokens.get("SUPABASE_URL")
-supabase_key = tokens.get("SUPABASE_KEY")
-
-supabase: Client = create_client(supabase_url, supabase_key)
+supabase = SupabaseClient()
 
 class ModeratorState(Enum):
     EVAL_START = auto()
@@ -55,36 +49,28 @@ class ModeratorReport:
         self.client = client
         self.original_message = message
         self.state = ModeratorState.EVAL_START
+        self.current_report = None
         self.reported_user_id = None
         self.reported_user_name = None
-        self.extract_reported_user_info()
-
-    def extract_reported_user_info(self):
-        lines = self.original_message.content.split('\n')
-        for line in lines:
-            if line.startswith("**Reported User:**"):
-                match = re.search(r'<@(\d+)>', line)
-                if match:
-                    self.reported_user_id = int(match.group(1))
-                match = re.search(r'\((.+?)\)', line)
-                if match:
-                    self.reported_user_name = match.group(1)
-                break
     
     async def handle_report(self, message):
         report = self.original_message
         replies = []
         
         if self.state == ModeratorState.EVAL_START:
+            next_report = supabase.fetch_next_report()
+            if not next_report:
+                return ["There are no active reports in the queue at the moment."]
+            self.current_report = next_report
+            self.reported_user_id = self.current_report['reported_user']
             self.state = ModeratorState.AWAITING_VERACITY_DECISION
-            # TODO queue to retrive next report
-            return [f"This is the highest priority report in the queue.",
+            return [self.format_report(next_report),
+                    f"This is the highest priority report in the queue.",
                     self.create_options_list("Is this a legit report?",
                                              self.VERACITY_OPTIONS)]
         
         if self.state == ModeratorState.AWAITING_VERACITY_DECISION:
             i = self.get_index(message, self.VERACITY_OPTIONS)
-            # self.reason.append(self.INITIAL_OPTIONS[i])
 
             if i == -1:
                 return ["Please enter a number corresponding to the given options."]
@@ -96,7 +82,8 @@ class ModeratorReport:
                 return [reply]
             elif i == 1:
                 self.state = ModeratorState.AWAITING_FALSE_REPORT_DECISION
-                reply = self.create_options_list("This is the number of times the user has submitted fake reports in the past. How would you like to proceed?",
+                num_false_reports = supabase.fetch_num_false_reports_submitted(self.current_report['reported_by'])
+                reply = self.create_options_list(f"@{self.current_report['reported_by']} has submitted {num_false_reports} fake reports in the past. How would you like to proceed?",
                                                  self.FALSE_REPORT_DECISION_OPTIONS)
                 return [reply]
             else:
@@ -116,9 +103,9 @@ class ModeratorReport:
 
             if i == 1:
                 self.state = ModeratorState.ACTION_COMPLETE
+                supabase.increment_num_false_reports_submitted(self.current_report['reported_by'])
                 reply = "We increased the number of false reports associated to this user. We are closing the report."
                 replies += [reply]
-                # TODO increase the number of times the user has submitted false reports
         
         if self.state == ModeratorState.AWAITING_LEGIT_REPORT_DECISION:
             i = self.get_index(message, self.LEGIT_REPORT_DECISION_OPTIONS)
@@ -148,7 +135,7 @@ class ModeratorReport:
 
             if i == 1:
                 self.state = ModeratorState.AWAITING_HIDE_CHILDREN_DECISION
-                # TODO increase number of times user has been reported
+                supabase.increment_num_reports_received(self.reported_user_id)
                 reply = self.create_options_list("We increased the number of times this user has been reported for CSAM. Would you like to hide child profiles from this user?",
                                                  self.YES_NO_OPTIONS)
                 return [reply]
@@ -170,8 +157,6 @@ class ModeratorReport:
             self.state = ModeratorState.EVAL_START
             return replies + ["Report completed."] 
 
-    # TODO: Trigger the appropriate action based on the commands below.
-
     def create_options_list(self, prompt, options):
         res = prompt
         for i, option in enumerate(options):
@@ -188,22 +173,19 @@ class ModeratorReport:
             return -1
         return i
     
-    async def handle_ban(self, message):
-        current_report = supabase.table('User').select('*').eq('current_report', True).execute()
-
-        if len(current_report.data) > 0:
-            report_data = current_report.data[0]
-            reported_user = report_data['reported_user']
-            reported_message = report_data['reported_message']
-            message_link = report_data['message_link']
-            message_channel = report_data['message_channel']
-
+    async def handle_ban(self, message):        
+        if self.current_report:
+            reported_user = self.current_report['reported_user']
+            reported_message = self.current_report['reported_message']
+            message_link = self.current_report['message_link']
+            message_channel = 'group-29'
             try:
+                supabase.ban_user(reported_user)
                 channel = discord.utils.get(self.client.get_all_channels(), name=message_channel)
                 if channel:
-                    await channel.send(f"User {reported_user} has been banned for the following message:\n```{reported_message}```\nMessage Link: {message_link}")
+                    await channel.send(f"User <@{reported_user}> has been banned for the following message:\n```{reported_message}```\nMessage Link: {message_link}")
 
-                    await message.channel.send(f"User {reported_user} has been successfully banned. The reporting user has been notified.")
+                    await message.channel.send(f"User <@{reported_user}> has been successfully banned. The reporting user has been notified.")
                 else:
                     await message.channel.send("Channel not found.")
             except Exception as e:
@@ -212,17 +194,16 @@ class ModeratorReport:
             await message.channel.send("No current report found.")
 
     async def handle_hide_profile(self, message):
-        current_report = supabase.table('User').select('*').eq('current_report', True).execute()
-
-        if len(current_report.data) > 0:
-            report_data = current_report.data[0]
-            reported_user = report_data['reported_user']
-            message_channel = report_data['message_channel']
+        if self.current_report:
+            reported_user = self.current_report['reported_user']
+            reported_message = self.current_report['reported_message']
+            message_link = self.current_report['message_link']
+            message_channel = 'group-29'
 
             try:
                 channel = discord.utils.get(self.client.get_all_channels(), name=message_channel)
                 if channel:
-                    await channel.send(f"Profile for user {reported_user} has been hidden.")
+                    await channel.send(f"Profile for user <@{reported_user}> has been hidden.")
                 else:
                     await message.channel.send("Channel not found.")
             except Exception as e:
@@ -231,21 +212,18 @@ class ModeratorReport:
             await message.channel.send("No current report found.")
 
     async def handle_escalate(self, message):
-        current_report = supabase.table('User').select('*').eq('current_report', True).execute()
-
-        if len(current_report.data) > 0:
-            report_data = current_report.data[0]
-            reported_user = report_data['reported_user']
-            reported_message = report_data['reported_message']
-            message_link = report_data['message_link']
-            message_channel = report_data['message_channel']
+        if self.current_report:
+            reported_user = self.current_report['reported_user']
+            reported_message = self.current_report['reported_message']
+            message_link = self.current_report['message_link']
+            message_channel = 'group-29'
 
             try:
                 channel = discord.utils.get(self.client.get_all_channels(), name=message_channel)
                 if channel:
-                    await channel.send(f"Report for user {reported_user} has been escalated to higher authorities.\nReported Message: ```{reported_message}```\nMessage Link: {message_link}")
+                    await channel.send(f"Report for user <@{reported_user}> has been escalated to higher authorities.\nReported Message: ```{reported_message}```\nMessage Link: {message_link}")
 
-                    await message.channel.send(f"Report for user {reported_user} has been successfully escalated. The reporting user has been notified.")
+                    await message.channel.send(f"Report for user <@{reported_user}> has been successfully escalated. The reporting user has been notified.")
                 else:
                     await message.channel.send("Channel not found.")
             except Exception as e:
@@ -257,4 +235,19 @@ class ModeratorReport:
         await message.channel.send("Report has been resolved.")
 
     def report_complete(self):
+        if self.current_report:
+            supabase.close_report(self.current_report['id'])
         return self.state == ModeratorState.ACTION_COMPLETE
+    
+    def format_report(self, data):
+        reasons = data['reasons'].split(',')
+        report_message = "**Report**\n\n"
+        report_message += f"**Priority:** {data['priority']}\n"
+        report_message += f"**Reported By:** <@{data['reported_by']}>\n"
+        report_message += f"**Reported User:** <@{data['reported_user']}>\n\n"
+        report_message += f"**Reported Message:**\n```{data['reported_message']}```\n"
+        report_message += f"**Message Link:** {data['message_link']}\n\n"
+        report_message += "**Reason(s):**\n"
+        for reason in reasons:
+            report_message += f"- {reason}\n"
+        return report_message
---
+++ DiscordBot/supabase_client.py
@@ -0,0 +1,111 @@
+import json
+from supabase import create_client, Client
+import discord
+
+with open("tokens.json", "r") as f:
+    tokens = json.load(f)
+
+SUPABASE_URL = tokens.get("SUPABASE_URL")
+SUPABASE_KEY = tokens.get("SUPABASE_KEY")
+
+class SupabaseClient:
+    def __init__(self) -> None:
+        self.client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    
+    def store_report(self, report):
+        reasons_text = ', '.join(report.reason)
+        data = {
+            'priority': report.priority,
+            'reported_user': str(report.message.author.id),
+            'reported_by': str(report.user_id),
+            'reported_message': report.message.content,
+            'message_link': report.message_link,
+            'reasons': reasons_text,
+        }
+        self.client.table('Reports').insert(data).execute()
+        self.increment_num_reports_received(report.message.author.id)
+        self.increment_num_reports_submitted(report.user_id)
+
+    def fetch_num_reports_received(self, user):
+        data, count = self.client.table('UserStats') \
+                .select('num_reports_received') \
+                .eq('user', user) \
+                .execute()
+        if data[1]:
+            return data[1][0]['num_reports_received']
+        else:
+            return 0
+
+    def fetch_num_reports_submitted(self, user):
+        data, count = self.client.table('UserStats') \
+                .select('num_reports_submitted') \
+                .eq('user', user) \
+                .execute()
+        if data[1]:
+            return data[1][0]['num_reports_submitted']
+        else:
+            return 0
+    
+    def fetch_num_false_reports_submitted(self, user):
+        data, count = self.client.table('UserStats') \
+                .select('num_false_reports_submitted') \
+                .eq('user', user) \
+                .execute()
+        if data[1]:
+            return data[1][0]['num_false_reports_submitted']
+        else:
+            return 0
+
+    def increment_num_reports_received(self, user):
+        num = self.fetch_num_reports_received(user)
+        self.client.table('UserStats') \
+                .upsert({'user': user, 'num_reports_received': num + 1}) \
+                .execute()
+
+    def increment_num_reports_submitted(self, user):
+        num = self.fetch_num_reports_submitted(user)
+        self.client.table('UserStats') \
+                .upsert({'user': user, 'num_reports_submitted': num + 1}) \
+                .execute()
+    
+    def increment_num_false_reports_submitted(self, user):
+        num = self.fetch_num_false_reports_submitted(user)
+        self.client.table('UserStats') \
+                .upsert({'user': user, 'num_false_reports_submitted': num + 1}) \
+                .execute()
+
+    def fetch_total_num_reports(self):
+        data, count = self.client.table('Reports') \
+                .select('*', count='exact') \
+                .eq('closed', False) \
+                .execute()
+        return count[1]
+        
+
+    def fetch_next_report(self):
+        try:
+            data, count = self.client.table('Reports') \
+                    .select('*') \
+                    .order('priority') \
+                    .order('created_at') \
+                    .eq('closed', False) \
+                    .limit(1) \
+                    .single() \
+                    .execute()
+            return data[1]
+        except:
+            return None
+
+    def close_report(self, report_id):
+        data, count = self.client.table('Reports') \
+                .update({'closed': True}) \
+                .eq('id', report_id) \
+                .execute()
+        
+    def ban_user(self, user):
+        data, count = self.client.table('UserStats') \
+                .update({'banned': True}) \
+                .eq('user', user) \
+                .execute()
+        
+
