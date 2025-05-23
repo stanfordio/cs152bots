59f5357c1d7a4ba3351a317a7fea324614f21ac6
Teddy Zhang <teddyzhang@DN0a1f69d2.SUNet>
2025-05-22 22:12:30 -0700

    minor cleanup

--- .history/DiscordBot/submitted_report_20250509125057.py
+++
@@ -1,7 +0,0 @@
-class SubmittedReport:
-    def __init__(self, author, content, type, subtype, imminent):
-        self.author = author
-        self.content = content
-        self.type = type
-        self.subtype = subtype
-        self.imminent = imminent
